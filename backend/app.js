fcca82533d0f9b02ab237b8abeb4a7fcfc53ccaa
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-20 22:52:25 +0000

    crud routes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,70 @@
 import express from 'express'
-import { readFileSync } from 'node:fs'
+import { readFileSync, writeFile } from 'node:fs'
 
 const app = express()
 
-const tours = JSON.parse(readFileSync('./backend/data/tours-simple.json'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/api/v1/tours', (req, res) => {
+let tours = JSON.parse(readFileSync('./backend/data/tours-simple.json'))
+
+// Get All Tours
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours }
   })
-})
+}
+
+// Get Tour By Id
+const getTourById = (req, res) => {
+  const id = Number(req.params.id)
+  const tour = tours.find(tour => tour.id === id)
+  if (!tour)
+    return res.status(400).json({ status: 'fail', message: 'Invalid ID' })
+  res.status(200).json({
+    status: 'success',
+    data: { tour }
+  })
+}
+
+// Create New Tour
+const createTour = (req, res) => {
+  const id = tours[tours.length - 1].id + 1
+  const tour = { id, ...req.body }
+  tours = [...tours, tour]
+  writeFile('./backend/data/tours-simple.json', JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: { tour }
+    })
+  })
+}
+
+// Update Tour By Id
+const updateTour = (req, res) => {
+  if (Number(req.params.id) > tours.length)
+    return res.status(400).json({ status: 'fail', message: 'Invalid ID' })
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '<Updated Tour Here>' } })
+}
+
+// Delete Tour By Id
+const deleteTour = (req, res) => {
+  if (Number(req.params.id) > tours.length)
+    return res.status(400).json({ status: 'fail', message: 'Invalid ID' })
+  res.status(204).json({ status: 'success', data: null })
+}
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTourById)
+  .patch(updateTour)
+  .delete(deleteTour)
 
 const port = 3000
 app.listen(port, () => {
